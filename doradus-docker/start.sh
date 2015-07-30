af068fdb0ce26d12e85201d523b4df508bf263cd
Tra Duong <tra.duong@software.dell.com>
2015-07-30 11:40:54 -0700

    remove the unused code

--- doradus-docker/start.sh
+++ doradus-docker/start.sh
@@ -9,4 +9,3 @@ cd /opt/doradus
 cd doradus-dynamodb
 
 java -cp $CLASSPATH $JVM_PARAMS com.dell.doradus.core.DoradusServer $PARAMS
-#java -cp ./target/classes:./target/dependency/*:../doradus-jetty/target/classes:../doradus-jetty/target/dependency/* -Xmx1G -DDDB_PROFILE_NAME=doradus -Dddb.region=us-west-1 com.dell.doradus.core.DoradusServer -dbservice com.dell.doradus.db.dynamodb.DynamoDBService -restport $PORT
